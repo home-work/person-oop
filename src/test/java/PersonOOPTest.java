dcfa0092c2246740016e6ed4724e86cd9d34734d
datdta95 <datdta95@gmail.com>
2018-08-30 05:53:55 +0700

    test: PASS with class Student

--- src/test/java/PersonOOPTest.java
+++ src/test/java/PersonOOPTest.java
@@ -10,4 +10,12 @@ public class PersonOOPTest {
         String actual = person.toString();
         assertEquals(expect, actual);
     }
+
+    @Test
+    public void testClassStudent() {
+        Student person = new Student("John", "USA", "A", 2, 2.0);
+        String expect = "Student has name: John address: USA program: A year: 2 fee: 2.0";
+        String actual = person.toString();
+        assertEquals(expect, actual);
+    }
 }
